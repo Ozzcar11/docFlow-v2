1ff5b43ebd66ed18c1c928d60f6f5275f32867ac
Atav <atav.daniyalov@kirarock.team>
2024-10-27 21:49:26 +0300

    feat: Добавил поддержку типов

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,9 @@ module.exports = {
 
     'prefer-const': 'warn',
 
-    '@typescript-eslint/no-unused-vars': 'off'
+    '@typescript-eslint/no-unused-vars': 'off',
+
+    'vue/multi-word-component-names': 'off'
 
     // '@typescript-eslint/ban-ts-comment': 'off',
     // 'vue/script-setup-uses-vars': 'error',

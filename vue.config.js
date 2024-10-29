1407837b2495a9f1c2ebf0365d582e2679688985
Atav <atav.daniyalov01@gmail.com>
2024-10-29 16:51:41 +0300

    fix: Исправил ошибку с hidratation mismatch

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
-module.exports = {
-    publicPath: '',
-    configureWebpack: {
+const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
+
+module.exports = defineConfig({
+  configureWebpack: {
     plugins: [
       require('unplugin-auto-import/webpack')({ /* options */ }),
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
+      })
     ],
-  },
-}
+  }
+})
